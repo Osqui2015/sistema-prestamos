de4a9881df51d1dfed2bc7a0f665b7e3d4538930
Oscar Guerrero <68171383+Osqui2015@users.noreply.github.com>
2026-01-12 09:23:45 -0300

    Disable migration commands in start.sh
    Comment out migration commands to prevent execution.

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "--- Ejecutando Migraciones ---"
-php artisan migrate:fresh --force
+# echo "--- Ejecutando Migraciones ---"
+# php artisan migrate --force  <-- LE PONEMOS UN # AL PRINCIPIO PARA QUE NO SE EJECUTE
 
 echo "--- Iniciando Servidor ---"
 php artisan serve --host=0.0.0.0 --port=$PORT
