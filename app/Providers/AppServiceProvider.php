e64c2d002fcadb4a3bbbcb1c78e00decc4c45ce3
Oscar Guerrero <oscar.guerrero@suizoargentina.com>
2026-01-12 09:54:00 -0300

    Forzando HTTPS para arreglar Mixed Content

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Vite::prefetch(concurrency: 3);
+        // Forzar HTTPS en producción (Render)
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
